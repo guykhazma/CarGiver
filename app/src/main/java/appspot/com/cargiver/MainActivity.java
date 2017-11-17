0a0ec89db4bb58e37c347d7770f5ca24bbdddb43
guykhazma <guykhazma@mail.tau.ac.il>
2017-11-17 13:06:47 +0200

    Added Bluetooth manager code Began checking device bluetooth abilities

--- app/src/main/java/appspot/com/cargiver/MainActivity.java
+++ app/src/main/java/appspot/com/cargiver/MainActivity.java
@@ -1,8 +1,16 @@
 package appspot.com.cargiver;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +20,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = MainActivity.class.getName(); // TAG for logging
+
+    // Bluetooth
+    private final static int REQUEST_ENABLE_BT = 1; // for bluetooth request response code
+    private static boolean bluetooth_enabled = false;
+    private static boolean has_bluetooth = true;
+    private static BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +42,32 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        // If the adapter is null, then Bluetooth is not supported so update bluetooth icon
+        if (mBluetoothAdapter == null) {
+            Log.w(TAG, "Device has no bluetooth");
+            has_bluetooth = false;
+            // TODO: check if theme should be  context.getTheme() as in https://stackoverflow.com/questions/33140706/android-dynamically-change-fabfloating-action-button-icon-from-code
+            fab.setImageDrawable(getResources().getDrawable(R.drawable.stat_sys_data_bluetooth_disabled, null));
+            //fab.setBackgroundColor();
+        }
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // check for bluetooth support
+                if (!has_bluetooth) {
+                    Snackbar.make(view, "Oops...It seems your phone doesn't have Bluetooth", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+                else  {
+                    // bluetooth exist check if enabled
+                    if (!mBluetoothAdapter.isEnabled()) {
+                        // ask user to enable bluetooth
+                        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                    }
+                }
             }
         });
 
@@ -40,6 +79,26 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check response to bluetooth enable
+        if (requestCode == REQUEST_ENABLE_BT) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                bluetooth_enabled = true;
+
+            }
+            else {
+                Context context = getApplicationContext();
+                CharSequence text = "Failed Enabling Bluetooth";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        }
     }
 
     @Override
