e9ec4ad979e6fa2182cd1a1bcc06dcbfbfcf3816
michaeltahar <michael.tahar@gmail.com>
2017-11-29 12:40:40 +0200

    added some driver fragments michaeltahar

---
+++ app/src/main/java/appspot/com/cargiver/RouteListAdapter.java
@@ -0,0 +1,70 @@
+package appspot.com.cargiver;
+
+import android.widget.ArrayAdapter;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by MT on 27/11/2017.
+ */
+
+public class RouteListAdapter extends ArrayAdapter {
+    //to reference the Activity
+    private final Activity context;
+    //to store the list of routes
+    private final String[] RoutesNameArray;
+    private final List<Drives> RoutesList;
+
+    public RouteListAdapter(Activity context, String[] nameArrayParam, List<Drives> DrivesList){
+
+        super(context,R.layout.route_name , nameArrayParam);
+        this.context=context;
+        this.RoutesNameArray = nameArrayParam;
+        this.RoutesList = DrivesList;
+    }
+
+
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.route_name, null,true);
+
+        //this code gets references to objects in the route_name.xml file
+        TextView RouteDriverField = (TextView) rowView.findViewById(R.id.route_driver);
+        TextView RouteDateField = (TextView) rowView.findViewById(R.id.route_date);
+
+        //this code sets the values of the objects to values from the arrays
+        //todo michaeltah - take the info from db by position
+        RouteDriverField.setText("tmp");
+        RouteDateField.setText("tmp");
+
+        return rowView;
+
+    };
+
+}
--- app/src/main/java/appspot/com/cargiver/RoutesListFragment.java
+++ app/src/main/java/appspot/com/cargiver/RoutesListFragment.java
@@ -2,6 +2,7 @@ package appspot.com.cargiver;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,23 +17,69 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by MT on 27/11/2017.
  */
 
 public class RoutesListFragment extends Fragment {
-    private ArrayAdapter<String> newRoutesArrayAdapter;
+    public ListView RouteslistView;
+    public DatabaseReference TheRoutesDB;
+    public RoutesListFragment(){};
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.routes_list, container, false);
+        // set menu as selected on startup
+        NavigationView navigationView = (NavigationView) getActivity().findViewById(R.id.nav_view_driver);
+        navigationView.getMenu().getItem(1).setChecked(true);
 
-        // Initialize array adapters. One for already paired devices and one for new
-        newRoutesArrayAdapter= new ArrayAdapter<String>(getActivity(), R.layout.route_name);
+        //take last 20 routes from db. if less take less...
 
-        //todo micaheltah - how to add entries??????????????????????????
+        //michaeltah - take information from db
+        TheRoutesDB = FirebaseDatabase.getInstance().getReference();
+        //TheRoutesDB.child("drives");
+        //todo michaetlah - see if we need drives list for another use
+        final List<Drives> DrivesList = new ArrayList<Drives>(); //will keep the drives data
+//        final String[] nameArray; //keep the drivers ids
+        RouteslistView = (ListView) view.findViewById(R.id.routes_list_view);
+
+        TheRoutesDB.child("drives").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot Child: dataSnapshot.getChildren()) {
+                    DrivesList.add(Child.getValue(Drives.class));
+                }
+                String[] nameArray = new String[DrivesList.size()];
+                for(int i=0; i<DrivesList.size(); i++){
+                    nameArray[i] = DrivesList.get(i).currentDriverID;
+                }
+                RouteListAdapter MyAmazingAdapter = new RouteListAdapter(getActivity(), nameArray, DrivesList);
+                RouteslistView.setAdapter(MyAmazingAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        } );
+
         return view;
     }
+
 
 
 }
