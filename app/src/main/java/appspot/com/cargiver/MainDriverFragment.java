fd3dc937b4970e1ba51829c92d1eb9b735d9755c
guykhazma <guykhazma@mail.tau.ac.il>
2017-12-01 08:30:36 +0200

    Updated driver first page

--- app/src/main/java/appspot/com/cargiver/MainDriverFragment.java
+++ app/src/main/java/appspot/com/cargiver/MainDriverFragment.java
@@ -1,12 +1,15 @@
 package appspot.com.cargiver;
 
 import android.app.Fragment;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.NavigationView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 /**
@@ -14,6 +17,11 @@ import android.widget.TextView;
  */
 
 public class MainDriverFragment extends Fragment {
+
+    boolean startDrivePressed;
+    // progress dialog
+    private ProgressDialog mProgressDlg;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -24,18 +32,26 @@ public class MainDriverFragment extends Fragment {
         NavigationView navigationView = (NavigationView) getActivity().findViewById(R.id.nav_view_driver);
         navigationView.getMenu().getItem(0).setChecked(true);
 
+        ImageButton btnStartDrive = view.findViewById(R.id.btn_start_drive);
+        TextView Explain = view.findViewById(R.id.start_driving_explain);
+
+        // make sure to display the right option if data collection already started
+        if (startDrivePressed == true) {
+            btnStartDrive.setImageResource(R.drawable.havearrived);
+            Explain.setText("When you arrive at your destination, please click on \'I have arrived\'");
+        }
+
         getActivity().setTitle("Driver");
 
-        Button btnStartDrive = view.findViewById(R.id.driving_page);
+        btnStartDrive = view.findViewById(R.id.btn_start_drive);
 
         btnStartDrive.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick (View v) {
                 //if we want to end the route
-                Button tmp = v.findViewById(R.id.driving_page);
-                TextView Explain = v.getRootView().findViewById(R.id.start_driving_explain);
-
-                if (tmp.getText().equals("Stop Driving")){
+                ImageButton btnStartDrive = v.findViewById(R.id.btn_start_drive);
+                TextView Explain = getView().findViewById(R.id.start_driving_explain);
+                if (startDrivePressed){
                     Fragment ShowRouteRes = new RouteResultFragment();
                     // set parameters to fragment
                     Bundle bundle = new Bundle();
@@ -43,9 +59,22 @@ public class MainDriverFragment extends Fragment {
                     ShowRouteRes.setArguments(bundle);
                     getFragmentManager().beginTransaction().replace(R.id.fragment_container_driver, ShowRouteRes, ShowRouteRes.getClass().getSimpleName()).addToBackStack(null).commit();
                 }
-                tmp.setText("Stop Driving");
-                Explain.setText("When you arrive your destination, please click on Stop Driving");
-
+                else {
+                    mProgressDlg = new ProgressDialog(getActivity());
+                    mProgressDlg.setMessage("Starting Data Collection Engine...");
+                    mProgressDlg.setCancelable(false);
+                    mProgressDlg.show();
+                    btnStartDrive.setImageResource(R.drawable.havearrived);
+                    Explain.setText("When you arrive at your destination, please click on \'I have arrived\'");
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        public void run() {
+                            mProgressDlg.dismiss();
+                        }
+                    }, 2000);
+                }
+                // register press
+                startDrivePressed = !startDrivePressed;
             }
         });
 
