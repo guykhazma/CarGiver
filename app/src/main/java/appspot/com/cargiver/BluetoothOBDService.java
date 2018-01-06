1bd4dfc8fe6acd9bee66bd130da4b72d7a6ba97b
guykhazma <guykhazma@mail.tau.ac.il>
2018-01-06 11:58:44 +0200

    More changes to OBD GPS

--- app/src/main/java/appspot/com/cargiver/BluetoothOBDService.java
+++ app/src/main/java/appspot/com/cargiver/BluetoothOBDService.java
@@ -107,11 +107,7 @@ public class BluetoothOBDService extends Service implements SensorEventListener 
     int count = 1; //num of measurements
     int NumOfPunish = 0; //num of punishments
     float AverageSpeed = 0; //the average speed
-    double lastLongitude;
-    double lastLatitude;
-    // distance objects
-    Location temp;
-    Location temp2;
+    Location lastLocation;
 
     // broadcast tags
     public static String connectionFailedBroadcastIntent = "com.OBDService.ConnectionFailed";
@@ -209,12 +205,9 @@ public class BluetoothOBDService extends Service implements SensorEventListener 
         uid = intent.getStringExtra("userID");
         address = intent.getStringExtra("address");
 
-        // distance objects
-        temp = new Location(LocationManager.GPS_PROVIDER);
-        temp2 = new Location(LocationManager.GPS_PROVIDER);
         // make sure user has location permissions on
         locationRequest = new LocationRequest();
-        locationRequest.setInterval(4000);
+        locationRequest.setInterval(5000);
         locationRequest.setFastestInterval(2000);
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         // check for user permissions
@@ -586,8 +579,7 @@ public class BluetoothOBDService extends Service implements SensorEventListener 
                                 // set start time
                                 dbref.child("drives").child(driveKey).child("StartTimeStamp").setValue(-Calendar.getInstance().getTime().getTime());
                                 measRef.setValue(new Measurement(0, lat, longitude, 0));
-                                lastLatitude = lastLatitude;
-                                lastLongitude = longitude;
+                                lastLocation = location;
                             }
                             // if somehow error occurred
                             else {
@@ -643,19 +635,14 @@ public class BluetoothOBDService extends Service implements SensorEventListener 
                                                 count++;
                                                 // if current location is far then 20m then last location update
                                                 // avoid a case where we have points in radius less then 200 meters
-                                                temp.setLatitude(lat);
-                                                temp.setLongitude(longitude);
-                                                temp2.setLatitude(lastLatitude);
-                                                temp2.setLongitude(lastLongitude);
-                                                if (temp.distanceTo(temp2) > 20) {
+                                                if (isBetterLocation(location, lastLocation) && location.distanceTo(lastLocation) > 100) {
                                                     // put new in db and update
                                                     measRef.setValue(new Measurement(speed, lat, longitude, rpm));
-                                                    lastLongitude = longitude;
-                                                    lastLatitude = lat;
+                                                    lastLocation = location;
                                                 }
                                                 else {
                                                     Log.e(TAG, "in");
-                                                    measRef.setValue(new Measurement(speed, lastLatitude, lastLongitude, rpm));
+                                                    measRef.setValue(new Measurement(speed, lastLocation.getLatitude(), lastLocation.getLongitude(), rpm));
                                                 }
                                                 // update grade only if speed is above 0
                                                 if (speed > 0) {
@@ -741,5 +728,64 @@ public class BluetoothOBDService extends Service implements SensorEventListener 
             Grade=100;
         }
         return Grade;
+    }
+
+    private static final int TWO_MINUTES = 1000 * 60 * 2;
+
+    /** Determines whether one Location reading is better than the current Location fix
+     * @param location  The new Location that you want to evaluate
+     * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+     */
+    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+        if (currentBestLocation == null) {
+            // A new location is always better than no location
+            return true;
+        }
+
+        // Check whether the new location fix is newer or older
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+        boolean isNewer = timeDelta > 0;
+
+        // If it's been more than two minutes since the current location, use the new location
+        // because the user has likely moved
+        if (isSignificantlyNewer) {
+            return true;
+            // If the new location is more than two minutes older, it must be worse
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        if (location.getAccuracy() > 10)
+            return false;
+        
+        // Check if the old and new location are from the same provider
+        boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                currentBestLocation.getProvider());
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Checks whether two providers are the same */
+    private boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+            return provider2 == null;
+        }
+        return provider1.equals(provider2);
     }
 }
