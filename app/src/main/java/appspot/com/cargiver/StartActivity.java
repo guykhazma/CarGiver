97404ee5d0e657e74200fdf120c464d32d340e07
guykhazma <guykhazma@mail.tau.ac.il>
2018-02-19 12:09:31 +0200

    Login Issue fixed

--- app/src/main/java/appspot/com/cargiver/StartActivity.java
+++ app/src/main/java/appspot/com/cargiver/StartActivity.java
@@ -14,6 +14,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 /**
  * Created by Guy on 11/23/2017.
@@ -28,70 +29,65 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         /*---------------------Login Listener-------------------------------------*/
-        FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                final FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // keep data fresh
-                    DatabaseReference drives = FirebaseDatabase.getInstance().getReference("drives");
-                    drives.keepSynced(true);
-                    DatabaseReference drivers = FirebaseDatabase.getInstance().getReference("drivers");
-                    drivers.keepSynced(true);
-                    DatabaseReference supervisors = FirebaseDatabase.getInstance().getReference("supervisors");
-                    supervisors.keepSynced(true);
-                    DatabaseReference users = FirebaseDatabase.getInstance().getReference("users");
-                    users.keepSynced(true);
-                    DatabaseReference regtokens = FirebaseDatabase.getInstance().getReference("regTokens");
-                    regtokens.keepSynced(true);
-                    // user is already logged make sure it has type in db
-                    final DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
-                    dbRef.child("users").child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-                            // if user exists check type
-                            if (dataSnapshot.getValue() != null) {
-                                // parse object to User class
-                                User currentUser = dataSnapshot.getValue(User.class);
-                                // user is driver
-                                if (currentUser.type == User.DRIVER) {
-                                    // Load driver activity
-                                    startActivity(new Intent(getBaseContext(), MainDriverActivity.class));
-                                    finish();
-                                }
-                                else if (currentUser.type == User.SUPERVISOR) {
-                                    // Load supervisor activity
-                                    startActivity(new Intent(getBaseContext(), MainSuperActivity.class));
-                                    finish();
-                                }
-                                // User type is unknown meaning he still didn't choose
-                                else {
-                                    // redirect to choose activity
-                                    startActivity(new Intent(getBaseContext(), DriverOrSuperActivity.class));
-                                    finish();
-                                }
-                            }
-                            //  User doesn't exist redirect to Login Activity
-                            else {
-                                startActivity(new Intent(getBaseContext(), LoginActivity.class));
-                                finish();
-                            }
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // keep data fresh
+            DatabaseReference drives = FirebaseDatabase.getInstance().getReference("drives");
+            drives.keepSynced(true);
+            DatabaseReference drivers = FirebaseDatabase.getInstance().getReference("drivers");
+            drivers.keepSynced(true);
+            DatabaseReference supervisors = FirebaseDatabase.getInstance().getReference("supervisors");
+            supervisors.keepSynced(true);
+            DatabaseReference users = FirebaseDatabase.getInstance().getReference("users");
+            users.keepSynced(true);
+            DatabaseReference regtokens = FirebaseDatabase.getInstance().getReference("regTokens");
+            regtokens.keepSynced(true);
+            // user is already logged make sure it has type in db
+            final DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+            dbRef.child("regTokens").child(user.getUid()).setValue(FirebaseInstanceId.getInstance().getToken()); // save user's registration token
+            dbRef.child("users").child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // if user exists check type
+                    if (dataSnapshot.getValue() != null) {
+                        // parse object to User class
+                        User currentUser = dataSnapshot.getValue(User.class);
+                        // user is driver
+                        if (currentUser.type == User.DRIVER) {
+                            // Load driver activity
+                            startActivity(new Intent(getBaseContext(), MainDriverActivity.class));
+                            finish();
                         }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-                            Log.w(TAG, "DB failed - " + databaseError.getMessage());
-                            Toast toast = Toast.makeText(getApplicationContext(), "Error occurred please try later", Toast.LENGTH_SHORT);
-                            toast.show();
+                        else if (currentUser.type == User.SUPERVISOR) {
+                            // Load supervisor activity
+                            startActivity(new Intent(getBaseContext(), MainSuperActivity.class));
+                            finish();
                         }
-                    });
-                } else {
-                    // user is not logged redirecting to login activity
-                    startActivity(new Intent(getBaseContext(), LoginActivity.class));
-                    finish();
+                        // User type is unknown meaning he still didn't choose
+                        else {
+                            // redirect to choose activity
+                            startActivity(new Intent(getBaseContext(), DriverOrSuperActivity.class));
+                            finish();
+                        }
+                    }
+                    //  User doesn't exist redirect to Login Activity
+                    else {
+                        startActivity(new Intent(getBaseContext(), LoginActivity.class));
+                        finish();
+                    }
                 }
-            }
-        };
-        FirebaseAuth.getInstance().addAuthStateListener(authListener);
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Log.w(TAG, "DB failed - " + databaseError.getMessage());
+                    Toast toast = Toast.makeText(getApplicationContext(), "Error occurred please try later", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            });
+        } else {
+            // user is not logged redirecting to login activity
+            startActivity(new Intent(getBaseContext(), LoginActivity.class));
+            finish();
+        }
     }
 }
